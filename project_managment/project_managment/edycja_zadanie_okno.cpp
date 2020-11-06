b140ee998a5052e9cad21ea72d6f7c5bf203ce04
mateusz Marczewski <marczewski11111@wp.pl>
2020-11-06 10:37:51 +0100

    ostatnie zmiany przed prezentacją

--- project_managment/project_managment/edycja_zadanie_okno.cpp
+++ project_managment/project_managment/edycja_zadanie_okno.cpp
@@ -13,12 +13,22 @@ edycja_zadanie_okno::~edycja_zadanie_okno()
 void edycja_zadanie_okno::wczytaj_dane()
 {
 	QString nazwa = QString::fromStdString(Dane_zalogowanego_pracownika::instancja()->pobierz_nazwe_zadania());
-	
-	
-	
-	
-	
+	QString	data_roz = QString::fromStdString(Fun_projekty::pobierz_dane_zadania().pobierz_data_rozpoczecia());
+	QString	data_zak = QString::fromStdString(Fun_projekty::pobierz_dane_zadania().pobierz_data_zakonczenia());
+	QString	status = QString::fromStdString(Fun_projekty::pobierz_dane_zadania().pobierz_status());
+	QString	opis = QString::fromStdString(Fun_projekty::pobierz_dane_zadania().pobierz_opis());
+
+	ui.textEdit_opis->setText(opis);
 	ui.lineEdit_nazwa->setText(nazwa);
+	//TODO naprawic import daty
+	ui.dateEdit_start->setDate(QDate::fromString(data_roz, "yyy-MM-dd"));
+	ui.dateEdit_finish->setDate(QDate::fromString(data_zak, "yyy-MM-dd"));
+	if (status == "Rozpoczety")
+		ui.comboBox_status->setCurrentIndex(0);
+	else if (status == "Zawieszony")
+		ui.comboBox_status->setCurrentIndex(1);
+	else if (status == "Zamkniêty")
+		ui.comboBox_status->setCurrentIndex(2);
 }
 
 void edycja_zadanie_okno::on_pushButton_clicked()
@@ -40,6 +50,7 @@ void edycja_zadanie_okno::on_pushButton_potwierdz_clicked()
 	QString opis = ui.textEdit_opis->toPlainText();
 	
 	Fun_projekty::zaktualizuj_zadanie(nazwa.toStdString(), opis.toStdString(), start_date.toStdString(), finish_date.toStdString(), status.toStdString());
+	this->close();
 }
 
 void edycja_zadanie_okno::on_pushButton_usun_clicked()
